c6bf40f28b89a255b8b6cb6dc15ce864e6f3ea0a
Harsh Karande <harshskarande@gmail.com>
2021-06-11 18:59:08 +0530

    feat(page): create custom three-one grid

--- components/page/page.tsx
+++ components/page/page.tsx
@@ -6,7 +6,21 @@ interface Props {
 }
 
 const Page = ({ className, children }: Props): React.ReactElement => {
-  return <div className={`max-w-xl mx-auto p-5 ${className}`}>{children}</div>;
+  return (
+    <>
+      <div className={`max-w-xl mx-auto p-5 ${className}`}>{children}</div>
+      <style jsx>{`
+        .three-one {
+          grid-template-columns: 3fr 1fr;
+        }
+        @media screen and (max-width: 1024px) {
+          .three-one {
+            grid-template-columns: 1fr;
+          }
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Page;
