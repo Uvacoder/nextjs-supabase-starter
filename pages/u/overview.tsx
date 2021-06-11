df0a040a8acbddb2736afa0b42561cd4854cdc69
Harsh Karande <harshskarande@gmail.com>
2021-06-11 19:17:06 +0530

    feat(overview): create standard layout for overview

--- pages/u/overview.tsx
+++ pages/u/overview.tsx
@@ -1,11 +1,94 @@
+import { useEffect, useState } from 'react';
 import type { NextPage, GetServerSideProps } from 'next';
+import type { User as UserTypes } from '@supabase/supabase-js';
+import NextLink from 'next/link';
+import { Button, Card, Link, Text, User, Loading, useToasts } from '@geist-ui/react';
+import { Clock, CheckInCircle } from '@geist-ui/react-icons';
+import moment from 'moment';
 import { MetaHead } from '@/libs/components/.';
-import { supabase } from '@/supabase/.';
+import { Page } from '@/components/.';
+import { supabase, definitions } from '@/supabase/.';
 
-const Overview: NextPage = () => {
+interface Props {
+  user: UserTypes;
+}
+
+const Overview: NextPage<Props> = ({ user }: Props) => {
+  const [profile, setProfile] = useState<definitions['profile'] | null>(null);
+  const [, setToast] = useToasts();
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase.from<definitions['profile']>('profile').select();
+      if (error) return setToast({ text: error.message, type: 'error' });
+      if (data) setProfile(data[0]);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <MetaHead title="Overview" />
+      <Page className="grid three-one gap-5">
+        <div>
+          <Card>
+            <div className="py-8 flex flex-col justify-center items-center sm:items-start">
+              <Text h4 className="m-0">
+                You dont have any secrets.
+              </Text>
+              <Text
+                style={{ color: '#444' }}
+                className="mb-5 max-w-md text-sm text-center sm:text-left"
+              >
+                Get started with creating a new secret, add notes or description and secure it with
+                a personal identification number.
+              </Text>
+              <Button type="secondary">Create New Secret</Button>
+            </div>
+          </Card>
+          <div className="mt-5 grid">
+            <Text small>Explore more integrations and expand your account security.</Text>
+            <NextLink href="/u/settings">
+              <Link color underline className="text-sm">
+                Create Personal Identification Number &rarr;
+              </Link>
+            </NextLink>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <Text h5 className="m-0">
+            Recent Activity
+          </Text>
+          {profile ? (
+            <>
+              <User
+                src={`https://source.boringavatars.com/${profile?.avatar_type}/32/${user.id}`}
+                name={profile?.full_name}
+                style={{ padding: 0 }}
+              >
+                via {user.app_metadata.provider}
+              </User>
+              <div className="pl-2 flex items-center gap-3">
+                <Clock size={20} />
+                <Text small>{moment(user.last_sign_in_at).fromNow()}</Text>
+              </div>
+              <div className="pl-2 flex items-center gap-3">
+                <CheckInCircle size={20} />
+                <Text small>{moment(user.confirmed_at).format('LLL')}</Text>
+              </div>
+            </>
+          ) : (
+            <div className="h-5">
+              <Loading />
+            </div>
+          )}
+          <NextLink href="/u/activity">
+            <Link color underline className="text-sm">
+              View All Activity &rarr;
+            </Link>
+          </NextLink>
+        </div>
+      </Page>
     </>
   );
 };
