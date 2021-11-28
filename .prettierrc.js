81df6933e42ab2a0e8f3ee0586986a748774f19a
harshcut <harshskarande@gmail.com>
2021-11-28 15:09:20 +0530

    chore: remove deprecated prettier options

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,7 +6,6 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'es5',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
   arrowParens: 'always',
-  endOfLine: 'lf',
+  endOfLine: 'auto',
 };
